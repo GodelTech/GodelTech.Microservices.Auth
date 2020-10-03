a3311dcbd0622fb13e354a9c6091960c9e6711f0
Vladimir Rodchenko <vladimir@rodchenko.by>
2020-10-03 14:23:49 +0300

    add ClientCredentialsFlowTokenService

---
+++ src/GodelTech.Microservices.Auth/ClientCredentialsFlowTokenOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace GodelTech.Microservices.Auth
+{
+    /// <summary>
+    /// Client credentials flow token options.
+    /// </summary>
+    public class ClientCredentialsFlowTokenOptions
+    {
+        /// <summary>
+        /// Authority.
+        /// </summary>
+        /// <value>Authority.</value>
+        public string Authority { get; set; }
+
+        /// <summary>
+        /// Client id.
+        /// </summary>
+        /// <value>Client id.</value>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Client secret.
+        /// </summary>
+        /// <value>Client secret.</value>
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Scope.
+        /// </summary>
+        /// <value>Scope.</value>
+        public string Scope { get; set; }
+    }
+}
---
+++ src/GodelTech.Microservices.Auth/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace GodelTech.Microservices.Auth
+{
+    /// <summary>
+    /// Interface of token service.
+    /// </summary>
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Request token.
+        /// </summary>
+        /// <returns><cref>Task{TokenResponse}</cref>.</returns>
+        Task<TokenResponse> RequestTokenAsync();
+    }
+}
