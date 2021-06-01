614cec5cbaa1c9b253a6ec899212af09cee2b979
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-06-01 12:13:14 +0300

    add extension

---
+++ src/GodelTech.Microservices.Auth/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GodelTech.Microservices.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extensions to register ClientCredentialsFlowTokenService with the service collection.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register ClientCredentialsFlowTokenService with the service collection.
+        /// </summary>
+        /// <param name="services">Service collection.</param>
+        /// <param name="configureOptions">ClientCredentialsFlowTokenService options.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddClientCredentialsFlowTokenService(
+            this IServiceCollection services,
+            Action<ClientCredentialsFlowTokenOptions, IConfiguration> configureOptions)
+        {
+            // Options
+            services.AddOptions<ClientCredentialsFlowTokenOptions>()
+                .Configure(configureOptions);
+
+            // ClientCredentialsFlowTokenService
+            services.AddTransient<ITokenService, ClientCredentialsFlowTokenService>();
+
+            return services;
+        }
+    }
+}
